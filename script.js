d47fb5e26db1883fb01145e9b2dfc08872b995e2
Levchuk Taras <106012028+levchukt@users.noreply.github.com>
2024-09-06 15:51:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ if (input.value != ''){
 
 
 })
+
+let buy_btn = document.querySelectorAll('.btn')
+console.log(buy_btn)
+let form =document.querySelector('form')
+buy_btn.forEach((btn)=>{
+    btn.addEventListener('click', function(){
+        form.style.display= 'flex'
+        let close = document.querySelector('.close_form')
+        close.addEventListener('click',function(){
+        let rev=close.closest('form')
+        rev.style.display='none'})
+
+        let rikrol = document.querySelector('.form_btn')
+        rikrol.addEventListener('click', ()=>{
+            location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        })
+    })
+})
